f39a6f91e571384b685ad8fd92b567a86f76f9c0
Lorena1803 <lorena01ambrosio@gmail.com>
2022-10-17 20:54:38 -0300

    adicionei a tela de perfil

---
+++ GoReceitas/changeEmailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  changeEmailViewController.swift
+//  GoReceitas
+//
+//  Created by Lorena on 08/09/22.
+//
+
+import UIKit
+
+class changeEmailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
